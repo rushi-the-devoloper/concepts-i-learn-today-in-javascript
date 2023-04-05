53835f98ad8810eda799a679f832570c5d33befd
rushi-reactdev <rushikeshkale669@gmail.com>
2023-04-05 22:00:54 +0530

    i try to solve the examples of and operator and or operator in javascript

---
+++ script.js
@@ -0,0 +1,44 @@
+// and operator example
+// example number 1
+let age = 19;
+let category = "adult";
+if(age>18 && category=== "adult"){
+    console.log('USER CAN VISIT ANY WEBSITE');
+}
+else{
+    console.log('user cannot visit restricted sites');
+}
+// example number 2
+let accountType = "salleried";
+let user = "senior citizen";
+
+if(accountType=== "saving" && user === "senior citizen"){
+    console.log("USER WILL GET THE 10% INTREST ON FIXED DEPOSIT");
+
+}
+else{
+    console.log("user will get the 4% intrest on fixed deposit");
+}
+
+// OR operator 
+// Example Number 1
+ let employHaveSkills = "HTML CSS JAVASCRIPT "
+ let employIsLearningSkills = "no"
+ if(employHaveSkills=== "HTML CSS JAVASCRIPT " || employIsLearningSkills=== "yes" ){
+    console.log('WE CAN HIRE YOU');
+ }
+ else{
+    console.log('WE WILL INFORM YOU');
+ }
+
+ // or operator example no 2
+ let personOnBike= 2;
+ let haveLiscence = "no";
+ if(personOnBike>2 || haveLiscence==="yes"){
+    console.log("you break the rule please pay fine")
+ }
+else{
+    console.log('YOU ARE A HONORABLE CITIZEN')
+}
+// 
+
